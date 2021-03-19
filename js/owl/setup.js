dc324d44d5758b08cad15f99d40834060cc12875
Kris <krislane_oliveira21@hotmail.com>
2021-03-19 19:07:23 -0300

    initial commit

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+/* Referência JQuery do owl carousel */
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
